f6a5990f9fd91f91b6ecd1c3bdcefcbde1227fb8
Noon-R <noon.ryou0304@gmail.com>
2022-09-02 07:52:19 +0900

    sample textTemplate

---
+++ ClassGenerator/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ClassGenerator.TextTamplate;
+
+namespace ClassGenerator
+{
+    public class Entry
+    {
+        public static void Main() {
+            SampleClassTemplate sample = new SampleClassTemplate();
+            int[,] testData = new int[,] {
+                { 1,2,1,2,1},
+                { 2,3,2,3,2},
+                { 3,4,3,4,3},
+            };
+            sample.setData(testData);
+
+            using (StreamWriter sw = new StreamWriter("./test.cs", false, Encoding.UTF8))
+            {
+                sw.Write(sample.TransformText());
+            }
+
+        }
+    }
+}
---
+++ ClassGenerator/WorkSpace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassGenerator.TextTamplate
+{
+
+    public partial class SampleClassTemplate : SampleClassTemplateBase {
+
+
+        int[,] _Data;
+
+        public void setData(int[,] data) {
+            _Data = data;
+
+            
+        }
+
+
+
+    }
+
+
+}
